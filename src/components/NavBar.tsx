c827b3e2bf51a3c2bad11b3d07cb058b2c98d9d5
MeganSteenkamp <megan.steenkamp@gmail.com>
2021-04-23 08:36:00 +1200

    login and register working as expected with navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,39 @@
+import { Box, Button, Flex, Link } from '@chakra-ui/react';
+import React from 'react';
+import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    // user is not logged in
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg={'blue.300'} p={4}>
+      <Box ml={'auto'}>{body}</Box>
+    </Flex>
+  );
+};
