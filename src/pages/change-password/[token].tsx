6ddfedaff020a255790ff2a1042dcc652e7d4eb3
MeganSteenkamp <megan.steenkamp@gmail.com>
2021-04-27 16:27:39 +1200

    forgot/update password functionality with jwt added

---
+++ src/pages/change-password/[token].tsx
@@ -0,0 +1,54 @@
+import { Box, Button } from '@chakra-ui/react';
+import { Formik, Form } from 'formik';
+import { NextPage } from 'next';
+import router from 'next/router';
+import React from 'react';
+import { InputField } from '../../components/InputField';
+import { Wrapper } from '../../components/Wrapper';
+import { toErrorMap } from '../../utils/toErrorMap';
+import login from '../login';
+
+const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ newPassword: '' }}
+        onSubmit={async (values, { setErrors }) => {
+          // const response = await login(values);
+          // if (response.data?.login.errors) {
+          //   setErrors(toErrorMap(response.data.login.errors));
+          // } else if (response.data?.login.user) {
+          //   router.push('/');
+          // }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="newPassword"
+              placeholder="new password"
+              label="New Password"
+              type="password"
+            />
+            <Button
+              mt={4}
+              type="submit"
+              colorScheme="teal"
+              isLoading={isSubmitting}
+            >
+              change password
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+ChangePassword.getInitialProps = ({ query }) => {
+  return {
+    token: query.token as string,
+  };
+};
+
+export default ChangePassword;
