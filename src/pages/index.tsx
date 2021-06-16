3c84b7d431881bad72ff50130bace578bde7e4a2
MeganSteenkamp <megan.steenkamp@gmail.com>
2021-06-16 12:01:08 +1200

    add chakra ui nolines and istruncated'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ import React, { useState } from 'react';
 
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 10,
+    limit: 15,
     cursor: null as null | string,
   });
   const [{ data, fetching }] = usePostsQuery({
@@ -42,8 +42,15 @@ const Index = () => {
         <Stack spacing={8}>
           {data!.posts.posts.map((p) => (
             <Box key={p.id} p={5} shadow='md' borderWidth='1px'>
-              <Heading fontSize='xl'>{p.title}</Heading>
-              <Text mt={4}>{p.textSnippet}</Text>
+              <Heading fontSize='xl' isTruncated>
+                {p.title}
+              </Heading>
+              <Text fontSize='xs' as='i'>
+                posted by {p.creator.username}
+              </Text>
+              <Text mt={4} noOfLines={3}>
+                {p.text}
+              </Text>
             </Box>
           ))}
         </Stack>
