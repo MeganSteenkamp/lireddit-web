3dba38665cf14864a8994e6ab94d7a01a9d0b81e
MeganSteenkamp <megan.steenkamp@gmail.com>
2021-07-14 16:18:39 +1200

    add delete post confirmation modal

--- src/components/EditDeletePostButtons.tsx
+++ src/components/EditDeletePostButtons.tsx
@@ -1,10 +1,21 @@
 import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
-import { Flex, IconButton, Link } from '@chakra-ui/react';
-import NextLink from 'next/link';
-import React from 'react';
 import {
-  useDeletePostMutation
-} from '../generated/graphql';
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import router, { useRouter } from 'next/router';
+import React, { useRef } from 'react';
+import { useDeletePostMutation } from '../generated/graphql';
 
 interface EditDeletePostButtonsProps {
   id: number;
@@ -13,32 +24,72 @@ interface EditDeletePostButtonsProps {
 export const EditDeletePostButtons: React.FC<EditDeletePostButtonsProps> = ({
   id,
 }) => {
+  const router = useRouter();
   const [deletePost] = useDeletePostMutation();
+  const [isOpen, setIsOpen] = React.useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = useRef(null);
 
   return (
-    <Flex>
-      <NextLink href='/post/edit/[id]' as={`/post/edit/${id}`} passHref>
+    <>
+      <Flex>
+        <NextLink href='/post/edit/[id]' as={`/post/edit/${id}`} passHref>
+          <IconButton
+            as={Link}
+            rounded='md'
+            aria-label='edit post'
+            icon={<EditIcon />}
+            mr={2}
+          ></IconButton>
+        </NextLink>
         <IconButton
-          as={Link}
           rounded='md'
-          aria-label='edit post'
-          icon={<EditIcon />}
-          mr={2}
+          onClick={() => {
+            setIsOpen(true);
+          }}
+          aria-label='delete post'
+          icon={<DeleteIcon />}
         ></IconButton>
-      </NextLink>
-      <IconButton
-        rounded='md'
-        onClick={() => {
-          deletePost({
-            variables: { id },
-            update: (cache) => {
-              cache.evict({ id: `Post:${id}` });
-            },
-          });
-        }}
-        aria-label='delete post'
-        icon={<DeleteIcon />}
-      ></IconButton>
-    </Flex>
+      </Flex>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Delete Post
+            </AlertDialogHeader>
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button
+                colorScheme='red'
+                onClick={() => {
+                  deletePost({
+                    variables: { id },
+                    update: (cache) => {
+                      cache.evict({ id: `Post:${id}` });
+                    },
+                  });
+                  if (router.pathname !== '/') {
+                    router.push('/');
+                  }
+                }}
+                ml={3}
+              >
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 };
